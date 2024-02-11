e64034675a6ff69572659005430f10aefe1ef47f
Soe Pyae Moe <jre.soepyae.com>
2024-02-11 17:28:11 +0630

    add new api route for profile

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -15,10 +15,13 @@ class ProfileResource extends JsonResource
     public function toArray(Request $request): array
     {
         $image = '';
+        $hasImage;
         if ($this->image == null) {
             $image = "https://ui-avatars.com/api/?name=" . $this->name . "&color=random&background=random";
+            $hasImage = false;
         } else {
             $image = $this->image;
+            $hasImage = true;
         }
         return [
             'id' => $this->id,
@@ -26,8 +29,9 @@ class ProfileResource extends JsonResource
             'email' => $this->email,
             'phone' => $this->phone,
             'image' => $image,
-            'created_at' => $this->created_at->format('d-M-Y H:i'),
-            'updated_at' => $this->updated_at->format('d-M-Y H:i'),
+            'hasImage' => $hasImage,
+            'created_at' => $this->created_at->format('d-M-Y'),
+            'updated_at' => $this->updated_at->format('d-M-Y'),
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,57 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    // logout
     Route::post('logout', [AuthController::class, 'logout']);
+
+    // home data
     Route::get('home', [PageController::class, 'home']);
+
+    // profile data
     Route::get('profile', [PageController::class, 'profile']);
+
+    // products lists
     Route::get('products', [PageController::class, 'products']);
+
+    // product detail
     Route::get('products/{id}', [PageController::class, 'productDetail']);
+
+    // categories list
     Route::get('categories', [PageController::class, 'category']);
+
+    // product list in cart
     Route::get('cart', [PageController::class, 'cart']);
+
+    // remove product in cart
     Route::get('cart/remove/{id}', [PageController::class, 'removeCart']);
+
+    // checkout product in cart
     Route::get('checkout', [PageController::class, 'checkout']);
+
+    // order list and status
     Route::get('orders', [PageController::class, 'order']);
+
+    // increase product quantity in cart list
     Route::post('products/addqty/{id}', [PageController::class, 'addQty']);
+
+    // decrease product quantity in cart list
     Route::post('products/decqty/{id}', [PageController::class, 'decQty']);
+
+    // sent message to contact with admin
     Route::post('contact', [PageController::class, 'contact']);
+
+    // change password
+    Route::post('change_password', [AuthController::class, 'changePassword']);
+
+    // upload photo
+    Route::post('upload_photo', [PageController::class, 'uploadPhoto']);
+
+    // profile update
+    Route::post('profile/update', [PageController::class, 'profileUpdate']);
+
+    // check password
+    Route::post('check_password', [PageController::class, 'checkPassword']);
+
+    // delete accound
+    Route::post('delete', [PageController::class, 'delete']);
 });
